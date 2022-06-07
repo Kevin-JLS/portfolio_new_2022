4c42de6e50e24f49cdbe09da0f29e7b93df1a344
Kevin <kevin.jollis33@gmail.com>
2022-06-08 00:44:30 +0200

    creation

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// Flickity slider JS
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {});
+
+// Sal for animation in scrolling
+sal({
+     threshold: 0.5,
+     once: true,
+});
+
+// Navigation sticky on scroll
+window.addEventListener('scroll', () => {
+     const navigation = document.querySelector('#nav');
+     navigation.classList.toggle('sticky', window.scrollY > 0)
+})
