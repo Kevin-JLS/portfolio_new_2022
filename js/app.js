54ee3309a193c6ca5460d321fb7f02b9988a5b97
Kevin <kevin.jollis33@gmail.com>
2022-06-08 00:50:58 +0200

    ajout DOMContentLoaded

--- js/app.js
+++ js/app.js
@@ -1,15 +1,19 @@
-// Flickity slider JS
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {});
+window.addEventListener("DOMContentLoaded", () => {
 
-// Sal for animation in scrolling
-sal({
-     threshold: 0.5,
-     once: true,
+     // Flickity slider JS
+     var elem = document.querySelector('.main-carousel');
+     var flkty = new Flickity( elem, {});
+
+     // Sal for animation in scrolling
+     sal({
+          threshold: 0.5,
+          once: true,
+     });
+
+     // Navigation sticky on scroll
+     window.addEventListener('scroll', () => {
+          const navigation = document.querySelector('#nav');
+          navigation.classList.toggle('sticky', window.scrollY > 0)
+     })
+
 });
-
-// Navigation sticky on scroll
-window.addEventListener('scroll', () => {
-     const navigation = document.querySelector('#nav');
-     navigation.classList.toggle('sticky', window.scrollY > 0)
-})
